2bef07695e39347e9eb121a825433709b88a9663
alwaysbaguette <alwaysbaguette@gmail.com>
2019-03-12 07:45:49 +0900

    morgan logger add

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import {GraphQLServer} from "graphql-yoga";
+import logger from 'morgan';
 
 const PORT = process.env.PORT || 4000;
 const typeDefs = `
@@ -16,6 +17,8 @@ const resolvers = {
 
 const server = new GraphQLServer({typeDefs,resolvers});
 
+server.express.use(logger("dev"));
+
 server.start({port:PORT},() =>{
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port http://localhost:${PORT}`);
 })
