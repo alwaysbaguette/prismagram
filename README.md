213868d3e1042a80780f9ff9c9f45776838eee53
alwaysbaguette <alwaysbaguette@gmail.com>
2019-03-15 04:20:47 +0900

    add comment

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Prismagram with express with Express + Prisma + React and React Native
 - [X] Request Secret
 - [X] Confirm Secret (Login)
 - [X] Like / Unlike a photo
-- [] Comment on a photo
+- [X] Comment on a photo
 - [] Search by user
 - [] Search by location
 - [] See user profile
---
+++ src/api/Comment/addComment/addComment.js
@@ -0,0 +1,27 @@
+import middlewares from "../../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+
+
+export default {
+     Mutation : {
+         addComment : async (_,args, {request})=>{
+             middlewares.isAuthenticated(request);
+             const {text,postId} = args;
+             const {user} = request;
+             const comment = prisma.createComment({
+                 user:{
+                     connect:{
+                         id:user.id
+                     }
+                 },
+                 post:{
+                     connect:{
+                         id:postId
+                     }
+                 },
+                 text
+             });
+             return comment;
+         }
+     }
+}
