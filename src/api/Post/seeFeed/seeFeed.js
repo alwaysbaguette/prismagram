26374fd89c700cbf2a264af5d40994ad1088cb0c
alwaysbaguette <alwaysbaguette@gmail.com>
2019-03-17 03:09:12 +0900

    seeFeed

---
+++ src/api/Post/seeFeed/seeFeed.js
@@ -0,0 +1,21 @@
+import middlewares from "../../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Query :{
+        seeFeed: async (_,__,{request}) =>{
+            middlewares.isAuthenticated(request);
+            const { user } = request;
+            const following = await prisma.user({id:user.id}).following();
+            const posts = await prisma.posts({
+                where:{
+                    user:{
+                        id_in: [...following.map(user => user.id),user.id]
+                    }
+                },
+                orderBy:"createdAt_DESC"
+            });
+            return posts;
+        }
+    }
+}
